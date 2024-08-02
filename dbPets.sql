9ec062183a961669e483f7dd0e687565e88bd1a0
tsousacorreia <tsousacorreia@outlook.com>
2024-08-02 19:31:12 -0300

    script database

---
+++ dbPets.sql
@@ -0,0 +1,8 @@
+-- apagar banco de dados
+drop database dbPets;
+-- criar banco de dados
+create database dbPets;
+-- acessar o banco de dados
+use dbPets;
+-- visualizar banco de dados
+show databases;
