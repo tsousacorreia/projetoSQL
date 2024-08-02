569ab61d937748c5b7db0aa1c2a6ce31045e4442
tsousacorreia <tsousacorreia@outlook.com>
2024-08-02 19:51:15 -0300

    script database

--- dbPets.sql
+++ dbPets.sql
@@ -6,3 +6,21 @@ create database dbPets;
 use dbPets;
 -- visualizar banco de dados
 show databases;
+-- criar tabelas no banco de dados
+create table tbUsuarios(
+idUsuario int not null auto_increment,
+nome varchar(50) not null,
+senha varchar(10) not null,
+primary key(idUsuario)
+);
+create table tbFuncionarios(
+idFuncionarios int not null auto_increment,
+nome varchar(100) not null,
+email varchar(100),
+telCel char(11),
+primary key(idFuncionarios)
+);
+-- visualizar tabelas
+show tables;
+-- visualizar estrutura das tabelas
+desc tbUsuarios;
