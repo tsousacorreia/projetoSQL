5d433e3407fa5af5d8d5984ee47a407f4d8e46cb
tsousacorreia <tsousacorreia@outlook.com>
2024-08-09 19:21:40 -0300

    Atualizado em 09-08 as 19:21

--- dbPets.sql
+++ dbPets.sql
@@ -14,9 +14,12 @@ show databases;
 create table tbFuncionarios(
 idFuncionarios int not null auto_increment,
 nome varchar(100) not null,
-email varchar(100),
-telCel char(10),
-cpf char(14),
+cpf char(14) unique,
+email varchar(100) unique,
+sexo char(1) default 'F' check(sexo),
+salario decimal(9,2),
+nascimento date,
+telCelular char(10),
 primary key(idFuncionarios)
 );
 
