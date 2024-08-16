fb28cce70e1341a89e31cf022df988579db69c86
tsousacorreia <tsousacorreia@outlook.com>
2024-08-16 20:07:21 -0300

    Atualizado em 16/08/2024

--- dbCantina.sql
+++ dbCantina.sql
@@ -47,7 +47,6 @@ idFuncionario int not null,
 primary key(idUsuario),
 foreign key(idFuncionario)references tbFuncionarios(idFuncionario)
 );
-
 create table tbProdutos(
 idProduto int not null auto_increment,
 descricao varchar(100),
@@ -55,7 +54,7 @@ quantidade int,
 valor decimal(9,2) default 0 check(valor >=0),
 validade date,
 dataEntrada date,
-horaEntrada date,
+horaEntrada time,
 idFornecedor int not null,
 primary key(idProduto),
 foreign key(idFornecedor)references tbFornecedores(idFornecedor)
@@ -88,8 +87,28 @@ desc tbVendas;
 
 -- inserir registros nas tabelas
 
-insert into tbFuncionarios(nome,cpf,email,sexo,salario,nascimento,telCelular)values('Thiago','222.333.444-55','email@email.com','M',9000.00,'1989/05/14','99898-7675');
+insert into tbFuncionarios(nome,cpf,email,sexo,salario,nascimento,telCelular)values('Thiago','222.333.444-55','thiago@email.com','M',9000.00,'1989/05/14','99898-7675');
+insert into tbFuncionarios(nome,cpf,email,sexo,salario,nascimento,telCelular)values('Simone','111.333.555-77','simone@email.com','F',8200.00,'1986/03/21','99471-5478');
+insert into tbFornecedores(nome,cnpj,email)values('Loja Tem de Tudo','123.456.789-10','lojatdt@email.com');
+insert into tbFornecedores(nome,cnpj,email)values('Loja Super Oferta','234.567.891-11','lojasupo@email.com');
+insert into tbClientes(nome,email,telCelular)values('Juliana','juliana@email.com','95624-8753');
+insert into tbClientes(nome,email,telCelular)values('Mariana','mariana@email.com','98642-7891');
+insert into tbClientes(nome,email,telCelular)values('Lucas','lucas@email.com','95549-1245');
+insert into tbUsuarios(nome,senha,idFuncionario)values('Thiago','426153', 1);
+insert into tbUsuarios(nome,senha,idFuncionario)values('Simone','153456', 2);
+insert into tbProdutos(descricao,quantidade,valor,validade,dataEntrada,horaEntrada,idFornecedor)values('Garrafa Termica',100,10.00,'2034/12/01','2024/08/16','19:53:00',1);
+insert into tbProdutos(descricao,quantidade,valor,validade,dataEntrada,horaEntrada,idFornecedor)values('Ventilador',50,159.90,'2034/12/01','2024/08/16','19:54:00',1);
+insert into tbProdutos(descricao,quantidade,valor,validade,dataEntrada,horaEntrada,idFornecedor)values('TV Smart',80,4179.90,'2034/12/01','2024/08/16','19:55:00',2);
+insert into tbProdutos(descricao,quantidade,valor,validade,dataEntrada,horaEntrada,idFornecedor)values('Geladeira',30,6250.90,'2034/12/01','2024/08/16','19:56:00',2);
+insert into tbVendas(dataVenda,horaVenda,quantidade,idUsuario,idCliente,idProduto)values('2024/08/16','20:00',2,1,3,3);
+insert into tbVendas(dataVenda,horaVenda,quantidade,idUsuario,idCliente,idProduto)values('2024/08/16','20:03',10,2,1,1);
+insert into tbVendas(dataVenda,horaVenda,quantidade,idUsuario,idCliente,idProduto)values('2024/08/16','20:05',1,1,2,4);
 
 -- visualizar os registros das tabelas
 
 select * from tbFuncionarios;
+select * from tbFornecedores;
+select * from tbClientes;
+select * from tbUsuarios;
+select * from tbProdutos;
+select * from tbVendas;
